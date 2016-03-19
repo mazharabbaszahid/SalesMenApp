6cea8e8b227ad58d5177e91328552125fc693de8
mazhar <mazhar.abbass11@yahoo.com>
2016-03-19 18:46:22 +0500

    add myApp in GitHub

---
+++ myApp/www/js/app.js
@@ -0,0 +1,16 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+.config(function($urlRouterProvider,$stateProvider){
+  $stateProvider
+    .state('login',{
+      url:'/login',
+      templateUrl:'./templates/login.html',
+      controller:'loginController'
+    });
+    $urlRouterProvider.otherwise('login')
+  });
+
